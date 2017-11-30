86d62dcad6e663a728ba5ce60bc15bc444dfdad9
markpeou <markpeou@gmail.com>
2017-12-01 09:45:08 +1100

    thursday_homework

---
+++ Mark/wk01/4-thu/pt/pt.js
@@ -0,0 +1,12 @@
+// origin flagstaff station
+// destination kooyong station
+// write a for loop that displays the 4 stops that the passenger will be taking.
+//
+//
+var trip = ["Flagstaff","Melbourne Central","Parliament","Richmond","Kooyong","Tooronga"]
+
+for (var trip = 0; trip < 5; trip++) {
+  console.log(trip)
+}
+
+//console log is displaying the index numbers. need to figure out how to display the string value to notify user about the train stations in the journey. 
