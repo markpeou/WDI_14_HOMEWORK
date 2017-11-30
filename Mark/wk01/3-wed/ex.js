8d80367bab2aeac6d769839bf9384fb8c9544698
markpeou <markpeou@gmail.com>
2017-11-30 13:03:17 +1100

    add wednesday homework

---
+++ Mark/wk01/3-wed/ex.js
@@ -0,0 +1,15 @@
+var days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+console.log('days of the week: ' + days)
+
+var days2 = days.pop()
+days.unshift("sunday");
+console.log('days of the week: ' + days)
+
+console.log('new arrays of the week')
+
+// var weekend = days.push() [["monday","tuesday","wednesday","thursday","friday"]["saturday","sunday"]
+// // console.log(weekdaysVweekends)
+
+
+days.sort();
+console.log(days)
