dd5af18c4b31ec6d3462c3f082e98c7821747b26
markpeou <markpeou@gmail.com>
2018-02-11 11:26:19 +1100

    infinite scroll... but loading same images.

--- Mark/wk08/5-fri/giphy/app.js
+++ Mark/wk08/5-fri/giphy/app.js
@@ -6,16 +6,18 @@ $('button').on('click',function(event) {
   var $grid = $('.grid');
 
   var gif = {
-  url: 'http://api.giphy.com/v1/gifs/search?q='+ searchInput + '&api_key=kIzooqT0a3mcgRR8wXYXAvqJRNCDFXpY&offset=100'
+  url: 'http://api.giphy.com/v1/gifs/search?q='+ searchInput + '&api_key=kIzooqT0a3mcgRR8wXYXAvqJRNCDFXpY&limit=5&offset=10'
 };
 
 var results = $.ajax(gif).done(function(res) {
   res.data.forEach(function(gifs) {
     var newDivElem = $('<div>');
     var newImgElem = $('<img src=' + gifs.images.original.url +'>');
-
       $('.grid').append(newImgElem);
+      $(window).scroll(function() {
+        if ($(window).scrollTop() == $(document).height() - $(window).height())
+        $('.grid').append(newImgElem);
+      });
     });
   });
-
 });
