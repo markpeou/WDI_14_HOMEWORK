5fde754023ac2c868a5007a5e58aacf89e77e105
markpeou <markpeou@gmail.com>
2018-02-15 18:28:47 +1100

    updated days of the week

--- Mark/wk04/1-mon/days_of_the_week.rb
+++ Mark/wk04/1-mon/days_of_the_week.rb
@@ -8,11 +8,11 @@ days_of_the_week.pop
 days_of_the_week.unshift('Sunday')
 
 # 3. Create a new array of the days of the week:
-new_days = ["Saturday","Sunday",["Monday","Tuesday","Wednesday","Thursday","Friday"]]
+new_days= ['monday','tuesday','wednesday','thursday','friday',['saturday','sunday']]
 # 4. Remove either the weekdays or the weekends
 # Your choice...
-new_days.shift
-new_days.shift[0]
+new_days.slice(5,7)
+
 
 # 5. Sort the remaining days alphabetically
 new_days.sort
