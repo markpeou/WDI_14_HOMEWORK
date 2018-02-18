15edc084465e85fb9a36e4a8756683c4baa398f0
markpeou <markpeou@gmail.com>
2018-02-18 16:01:25 +1100

    infity scroll

--- Mark/wk08/5-fri/giphy/app.js
+++ Mark/wk08/5-fri/giphy/app.js
@@ -1,12 +1,15 @@
 console.log('boop');
 
+var gifs = 0
+var results = '';
+
 $('button').on('click',function(event) {
   event.preventDefault();
   var searchInput = $('#search').val()
   var $grid = $('.grid');
 
   var gif = {
-  url: 'http://api.giphy.com/v1/gifs/search?q='+ searchInput + '&api_key=kIzooqT0a3mcgRR8wXYXAvqJRNCDFXpY&limit=5&offset=10'
+  url: 'http://api.giphy.com/v1/gifs/search?q='+ searchInput + '&api_key=kIzooqT0a3mcgRR8wXYXAvqJRNCDFXpY&'
 };
 
 var results = $.ajax(gif).done(function(res) {
@@ -14,10 +17,14 @@ var results = $.ajax(gif).done(function(res) {
     var newDivElem = $('<div>');
     var newImgElem = $('<img src=' + gifs.images.original.url +'>');
       $('.grid').append(newImgElem);
-      $(window).scroll(function() {
-        if ($(window).scrollTop() == $(document).height() - $(window).height())
-        $('.grid').append(newImgElem);
-      });
     });
   });
+});
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() == $(document).height() - $(window).height())
+  var gif = {
+  url: 'http://api.giphy.com/v1/gifs/search?q='+ searchInput + '&api_key=kIzooqT0a3mcgRR8wXYXAvqJRNCDFXpY&limit=5&offset=' + gifs + results
+};
+  $('.grid').append(newImgElem);
 });
